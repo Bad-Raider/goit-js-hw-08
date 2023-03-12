8d3d972e7c1bef5a4a3c1074b3d2172e79f2de08
Bad-Raider <stanislav.gusiev@gmail.com>
2023-03-12 13:07:04 +0200

    hw #1 - done; hw #2 - done (90%).

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// it`s gallery DOM element
+const galleryContainer = document.querySelector(".gallery");
+// creating (rendering) card  
+function createElementMarkup(markup) {
+   return markup.map(({ description, preview, original }) => {
+    
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading="lazy" src=${preview} alt="${description}" />
+</a>
+    `
+}).join("")}
+// Edding card in the gallary
+galleryContainer.insertAdjacentHTML("beforeend",
+    createElementMarkup(galleryItems));
+// simple light box
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+    captionsData: "alt",
+    }
+);
+
+lightbox.on('show.simplelightbox'); 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let savedTimeInLocalStorage = parseInt(localStorage.getItem('videoplayer-current-time'));
+console.log(savedTimeInLocalStorage);
+
+player.on('timeupdate', function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+});
+
+player.setCurrentTime(savedTimeInLocalStorage).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
