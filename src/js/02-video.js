2158e2d2585ec1e1fb85fb9e89c11500de9d8fec
Bad-Raider <stanislav.gusiev@gmail.com>
2023-03-12 13:58:59 +0200

    hw #2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+// time saved in local storage
 let savedTimeInLocalStorage = parseInt(localStorage.getItem('videoplayer-current-time'));
-console.log(savedTimeInLocalStorage);
 
-player.on('timeupdate', function (data) {
+// handler time in local Storage 
+const handleCurrentTimeLocalStorage = (data) => {
     localStorage.setItem('videoplayer-current-time', data.seconds);
-});
+}
+// function follow up time and his save in local storage 
+player.on('timeupdate', throttle(handleCurrentTimeLocalStorage, 1000));
 
+// function restart video time saved in local storage
 player.setCurrentTime(savedTimeInLocalStorage).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
